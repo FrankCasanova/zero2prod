889724a9d3e83a086c990c94d87ef6dbcaacc445
Frank Casanova <frankcasanova.info@gmail.com>
2025-06-23 18:46:34 +0200

    feat: implement health check endpoint and refactor main function to use run

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health_check))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,6 @@
-// src/main.rs
+use zero2prod::run;
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
-
-// Handler function for incoming HTTP GET requests.
-// It takes an HttpRequest as input and returns something that implements the Responder trait.
-// This allows Actix Web to convert the return value into an HTTP response.
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     // Attempt to extract the "name" parameter from the request's path.
-//     // If the parameter is not present (e.g., the path is just "/"), default to "world".
-//     let name = req.match_info().get("name").unwrap_or("world");
-//     // Format a greeting message using the extracted or default name.
-//     // The resulting String will be sent as the HTTP response body.
-//     format!("Hello {}", &name)
-// }
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-// The main entry point for the Actix Web application.
-// The #[actix_web::main] macro sets up the async runtime required by Actix.
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // Create and configure the HTTP server.
-    // HttpServer::new takes a closure that returns an App instance for each worker thread.
-    HttpServer::new(|| {
-        // Construct a new Actix Web application.
-        App::new()
-            // Register a route for GET requests to the root path "/".
-            // This will call the `greet` handler function.
-            // .route("/", web::get().to(greet))
-            // Register a route for GET requests to "/{name}".
-            // The `{name}` part is a path parameter, which will be passed to the `greet` handler.
-            // .route("/{name}", web::get().to(greet))
-            .route("/health", web::get().to(health_check))
-    })
-    // Bind the server to the local address 127.0.0.1:8080.
-    .bind(("127.0.0.1", 8080))?
-    // Start the server and wait for it to complete.
-    .run()
-    .await
+async fn main() -> Result<(), std::io::Error> {
+    run().await
 }
