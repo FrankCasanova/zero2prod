dbc54aa2eca54158b39027ea6f7433cda7b066db
Frank Casanova <frankcasanova.info@gmail.com>
2025-06-24 16:01:32 +0200

    page 43

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,26 @@ use actix_web::dev::Server;
 use actix_web::{App, HttpResponse, HttpServer, web};
 use std::net::TcpListener;
 
+// We were returning 'impl Responder' at the very beginning.
+// we are no spelling out the type explicitly given that we have
+// become more familiar with 'actix-web'
+// There is no perfomrance difference! just a stylistic choice :)
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+// Let's start simple: we always return a 200 OK
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| 
-        App::new().route("/health", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,11 +2,22 @@ use std::net::TcpListener;
 use zero2prod::run;
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Port 0 is special-cased at the OS leve: trying to bind
+    // port 0 will trigger an OS scan for an available port
+    // which will then be bound to the application.
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+
     // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    let server = run(listener).expect("Failed to bind address");
+    let port = listener
+        .local_addr()
+        .unwrap()
+        .port();
+
+    let server = run(listener)
+        .expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
     // We return the application address to the caller!
     format!("http://127.0.0.1:{}", port)
 }
@@ -20,7 +31,7 @@ async fn health_check_works() {
     // Act
     let response = client
         // Use the returned application address
-        .get(&format!("{}/health", &address))
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -30,3 +41,55 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=Frank%20Casanova&email=frankcasanova.test%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failded to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = [
+        ("name=le%20guin", "missing the email"),
+        ("email=loco_loco%40gmail.com", "missing the name"),
+        ("", "missing both, name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to exxecute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail  with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
+}
