15ae1cc5a4d018af3e793c547ced01fd1b76d1ff
Frank Casanova <frankcasanova.info@gmail.com>
2025-07-01 14:18:53 +0200

    feat: refactor main and startup to include database connection in server setup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use sqlx::{Connection, PgConnection};
+use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     // We have removed the hard=code '8000' - it's now coming from our settings!
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let address = TcpListener::bind(address)?;
-    run(address)?.await
+    run(address, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,29 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{App, HttpResponse, HttpServer, web};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(
+    listener: TcpListener,
+    connection: PgConnection
+) -> Result<Server, std::io::Error> {
+
+    // Wrap the connection in a smart pointer cos the connection
+    // is not clonable, we need an Atomic Reference Counter 
+    // in order to allow al threads use that connection carefully
+    let connection = web::Data::new(connection);
+
+    // HttpServer does not take App as argument - it wanats a clousure
+    // that returns an App struct.
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+
+            // Register the connection as part of the applications state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
